6293bb5cd76af70aa739aac3173f7fd409e7f841
c-noblet <c.noblet.dev@gmail.com>
2020-06-16 01:38:59 +0200

    connect bot init

---
+++ app/components/TopBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <ActionBar 
+      title="Papagei" 
+      android:flat="true"
+    >
+      <ActionItem 
+        @tap="connectBot()"
+        ios.systemIcon="16" 
+        ios.position="right"
+        text="Connect Bot"
+        android.position="popup"
+      />
+    </ActionBar>
+</template>
+<script>
+import axios from 'axios';
+export default {
+  methods: {
+    connectBot(){
+      alert('test');
+    }
+  }
+}
+</script>
+<style scoped>
+  ActionBar {
+    background-color: #27ae60;
+    color: #ffffff;
+  }
+</style>
