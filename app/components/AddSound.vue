97d553d559b8f0923744d9b55ed527edc311caad
c-noblet <c.noblet.dev@gmail.com>
2020-06-16 21:31:34 +0200

    class removed

--- app/components/AddSound.vue
+++ app/components/AddSound.vue
@@ -1,21 +1,21 @@
 <template>
   <GridLayout rows="auto, *" columns="*">
-    <StackLayout class="form" row="0" col="0">
-      <StackLayout class="input-field">
-        <Label text="Titre" class="label font-weight-bold" />
-        <TextField class="input" v-model="form.title" />
+    <StackLayout row="0" col="0">
+      <StackLayout>
+        <Label text="Titre" />
+        <TextField v-model="form.title" />
       </StackLayout>
-      <StackLayout class="input-field">
-        <Label text="URL" class="label font-weight-bold" />
-        <TextField class="input" v-model="form.url" />
+      <StackLayout>
+        <Label text="URL" />
+        <TextField v-model="form.url" />
       </StackLayout>
-      <StackLayout class="input-field">
-        <Label text="Catégorie" class="label font-weight-bold" />
+      <StackLayout>
+        <Label text="Catégorie" />
         <ListPicker :items="categoryList" v-model="form.category" />
       </StackLayout>
       <GridLayout rows="auto, auto" columns="*, *">
-        <Button text="Save" @tap="onSave(form)" class="btn btn-primary" row="0" col="0" />
-        <Button text="Clear" @tap="onClear()" class="btn btn-primary" row="0" col="1" />
+        <Button text="Save" @tap="onSave(form)" row="0" col="0" />
+        <Button text="Clear" @tap="onClear()" row="0" col="1" />
       </GridLayout>
     </StackLayout>
   </GridLayout>
