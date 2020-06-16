506a7cccd97ba8c3bcf3047b987029d588c298f0
c-noblet <c.noblet.dev@gmail.com>
2020-06-16 22:38:34 +0200

    changed layout

--- app/components/AddSound.vue
+++ app/components/AddSound.vue
@@ -1,21 +1,21 @@
 <template>
   <GridLayout rows="auto, *" columns="*">
-    <StackLayout row="0" col="0">
-      <StackLayout>
-        <Label text="Titre" />
-        <TextField v-model="form.title" />
-      </StackLayout>
-      <StackLayout>
-        <Label text="URL" />
-        <TextField v-model="form.url" />
-      </StackLayout>
-      <StackLayout>
-        <Label text="Catégorie" />
-        <ListPicker :items="categoryList" v-model="form.category" />
-      </StackLayout>
-      <GridLayout rows="auto, auto" columns="*, *">
-        <Button text="Save" @tap="onSave(form)" row="0" col="0" />
-        <Button text="Clear" @tap="onClear()" row="0" col="1" />
+    <StackLayout class="nt-form" row="0" col="0">
+      <GridLayout class="m-l-10 m-t-30" rows="*" columns="auto,*">
+        <Label row="0" col="0" class="font-weight-bold p-t-15" text="Titre :" />
+        <TextField row="0" col="1" v-model="form.title" />
+      </GridLayout>
+      <GridLayout class="m-l-10 m-t-30" rows="*" columns="auto,*">
+        <Label row="0" col="0" class="font-weight-bold p-t-15" text="URL :" />
+        <TextField row="0" col="1" v-model="form.url" />
+      </GridLayout>
+      <GridLayout class="m-l-10 m-t-30" rows="*,*,*" columns="auto,*">
+        <Label row="1" col="0" class="m-r-15 font-weight-bold" text="Catégorie :" />
+        <ListPicker row="0" col="1" rowSpan="2" :items="categoryList" v-model="form.category" />
+      </GridLayout>
+      <GridLayout class="m-t-30" rows="auto, auto" columns="*, *">
+        <Button class="" text="Ajouter" @tap="onSave(form)" row="0" col="0" />
+        <Button class="" text="Effacer" @tap="onClear()" row="0" col="1" />
       </GridLayout>
     </StackLayout>
   </GridLayout>
