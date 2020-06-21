469deb3adb28cc69abea707c31fb5847a058da06
c-noblet <c.noblet.dev@gmail.com>
2020-06-21 17:21:09 +0200

    chaned component to be a modal

--- app/components/AddSound.vue
+++ app/components/AddSound.vue
@@ -1,38 +1,38 @@
 <template>
-  <GridLayout rows="auto, *" columns="*">
-    <StackLayout class="nt-form" row="0" col="0">
-      <GridLayout class="m-l-10 m-t-30" rows="*" columns="auto,*">
-        <Label row="0" col="0" class="font-weight-bold p-t-15" text="Titre :" />
-        <TextField row="0" col="1" v-model="form.title" />
-      </GridLayout>
-      <GridLayout class="m-l-10 m-t-30" rows="*" columns="auto,*">
-        <Label row="0" col="0" class="font-weight-bold p-t-15" text="URL :" />
-        <TextField row="0" col="1" v-model="form.url" />
-      </GridLayout>
-      <GridLayout class="m-l-10 m-t-30" rows="*,*,*" columns="auto,*">
-        <Label row="1" col="0" class="m-r-15 font-weight-bold" text="Catégorie :" />
-        <ListPicker row="0" col="1" rowSpan="2" :items="categoryList" v-model="form.category" />
-      </GridLayout>
-      <GridLayout class="m-t-30" rows="auto, auto" columns="*, *">
-        <Button class="" text="Ajouter" @tap="onSave(form)" row="0" col="0" />
-        <Button class="" text="Effacer" @tap="onClear()" row="0" col="1" />
-      </GridLayout>
-    </StackLayout>
-  </GridLayout>
+  <Frame id="addModal">
+    <Page>
+      <ActionBar title="Ajouter un son"/>
+      <FlexboxLayout class="p-10" justifyContent="space-around" flexDirection="column">
+          <GridLayout height="40" rows="*" columns="auto,*">
+            <Label row="0" col="0" class="font-weight-bold m-t-10" text="Titre :" />
+            <TextField row="0" col="1" v-model="form.title" />
+          </GridLayout>
+          <GridLayout height="40" class="m-t-30" rows="*" columns="auto,*">
+            <Label row="0" col="0" class="font-weight-bold m-t-10" text="URL :" />
+            <TextField row="0" col="1" v-model="form.url" />
+          </GridLayout>
+          <GridLayout height="180" rows="*,*,*" columns="auto,*">
+            <Label row="1" col="0" height="60" class="m-r-15 p-y-20 font-weight-bold" text="Catégorie :" />
+            <ListPicker row="0" col="1" rowSpan="3" :items="categoryList" v-model="form.category" />
+          </GridLayout>
+          <GridLayout height="60" rows="auto" columns="*, *">
+            <Button class="btn" text="Ajouter" @tap="$modal.close(form)" row="0" col="0" />
+            <Button class="btn" text="Fermer" @tap="$modal.close" row="0" col="1" />
+          </GridLayout>
+      </FlexboxLayout>
+    </Page>
+  </Frame>
 </template>
 <script>
 export default {
-  props: {
-    onSave: Function
-  },
+  name: 'AddSound',
   data() {
     return {
       categoryList: [
-        'Gj',
-        'Noob',
-        'Tg',
-        'Wtf',
-        'Autres'
+        'GJ',
+        'NOOB',
+        'TG',
+        'Autre'
       ],
       form: {
         title: '',
@@ -40,15 +40,11 @@ export default {
         category: 0
       }
     }
-  },
-  methods: {
-    onClear() {
-      this.form = {
-        title: '',
-        url: '',
-        category: 0
-      }
-    }
-  },
+  }
 };
 </script>
+<style scoped>
+  TextField, label {
+    font-size: 15;
+  }
+</style>
